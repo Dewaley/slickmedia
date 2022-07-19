301614752914586a588cb75dacd40b9da1fbe8ec
Dewaley <adewaleademilua@gmail.com>
2022-07-19 14:38:26 +0100

    Header done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
-    <h1 className='text-3xl'>Hello World</h1>
+    <div>
+      <Header/>
+    </div>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='w-screen bg-[#292929] py-4 px-8 flex justify-center md:justify-start'>
+      <h1 className='text-white border-[1px] border-white p-1 w-fit'>My Test App</h1>
+    </div>
+  );
+}
+
+export default Header
